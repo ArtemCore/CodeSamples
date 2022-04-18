0ba9c54209f987bd7f6613fa0720af5f87794d2d
Artem Tsitsvira <artem.t@core-tech.ru>
2022-04-18 16:18:16 +0300

    Added 02 code sample

---
+++ 02/create_key_pairs.py
@@ -0,0 +1,16 @@
+from bitcoin_key_gen import private_key, get_bitcoin_address
+
+
+def get_key_pairs():
+    """
+    simple function to create  private key based on secp256k1 curve and then create bitcoin address based on this key
+    :return: private key(str), bitcoin_address(str)
+    """
+    priv_key = private_key()
+    bitcoin_address = get_bitcoin_address(priv_key)
+    return priv_key, bitcoin_address
+
+
+for i in range(100):
+    priv_key, bitcoin_address = get_key_pairs()
+    print(bitcoin_address)
